5198a3161ae307409d6fb4c2811c08a051873e11
Abbas Oyetunde <abbasoyetunde@gmail.com>
2026-01-07 19:23:22 +0100

    lets see

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,31 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import {resumes} from "~/constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Joblyt" },
+    { name: "description", content: "Make your resume job-ready." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+        <Navbar/>
+        <section className="main-section">
+            <div className="page-heading">
+                <h1>Optimize Your Resume for Every Job</h1>
+                <h2>Get AI-powered insights to match your resume to the role..</h2>
+            </div>
+        </section>
+
+        {resumes.length > 0 && (
+            <div className="resumes-section">
+                {resumes.map((resume) => (
+                    <ResumeCard key={resume.id} resume={resume} />
+                ))}
+            </div>
+        )}
+    </main>
 }
